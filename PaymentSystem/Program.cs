f3e512f0e5e913c9fa2fda8beeb33d8c4c017965
maliuyanik <darkpassanger@darkpassangers-MacBook-Air.local>
2025-12-07 16:59:32 +0300

    dependency injection and credit card payment test

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ILogger logger = new ConsoleLogger();
+        INetworkConnection network = new MockNetworkAdapter();
+        
+        var cardPayment = new CreditCardPayment("1234567890123456", "123", 5000, logger, network);
+        cardPayment.ProcessPayment(1500);
             
     }
 }
